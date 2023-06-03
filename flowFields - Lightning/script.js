00f37d8c922cb99e59550f8b068b1e08ebc9472f
Alex Fisher <a.fisher615@gmail.com>
2023-06-03 09:14:19 +0900

    created initial spirals template

--- flowFields - Lightning/script.js
+++ flowFields - Lightning/script.js
@@ -6,8 +6,32 @@ console.log(ctx);
 
 //Global settings;
 ctx.lineWidth = 10;
-ctx.strokeStyle = "magenta";
+// ctx.strokeStyle = "magenta";
 
+//canvas shadow
+ctx.shadowOffsetX = 2;
+ctx.shadowOffsetY = 2;
+
+const grad1 = ctx.createLinearGradient(0,0, canvas.width, canvas.height)
+grad1.addColorStop('0.2', 'pink');
+grad1.addColorStop('0.3', 'red');
+grad1.addColorStop('0.4', 'orange');
+grad1.addColorStop('0.5', 'yellow');
+grad1.addColorStop('0.6', 'green');
+grad1.addColorStop('0.7', 'turquoise');
+grad1.addColorStop('0.8', 'violet');
+
+const grad2 = ctx.createRadialGradient(canvas.width * 0.5, canvas.height * 0.5, 30, canvas.width * 0.5, canvas.height * 0.5, 200,)
+grad2.addColorStop('0.4', 'orange');
+grad2.addColorStop('0.6', 'blue');
+grad2.addColorStop('0.5', 'pink');
+
+//Image pattern background
+const patternImg = document.getElementById('patternImg');
+console.log(patternImg);
+const pattern1 = ctx.createPattern(patternImg, 'no-repeat');
+
+ctx.strokeStyle = pattern1;
 
 class Line {
 
@@ -23,14 +47,14 @@ class Line {
         this.speedX = Math.random() * 1 - 0.5;
         // this.speedY = Math.random() * 5 - 2.5;
         this.speedY = 7;
-        this.lifeSpan = this.maxLength * 3;
+        this.lifeSpan = this.maxLength * 2;
         this.timer = 0;
 
     }
 
     draw(context) {
 
-        context.strokeStyle = `hsl(${this.hue}, 100%, 50%)`;
+        // context.strokeStyle = `hsl(${this.hue}, 100%, 50%)`;
         context.lineWidth = this.lineWidth;
         context.beginPath();
         context.moveTo(this.history[0].x, this.history[0].y);
@@ -46,8 +70,8 @@ class Line {
     update() {
         this.timer++;
         if (this.timer < this.lifeSpan) {
-            this.x += this.speedX + Math.random() * 50 - 25;
-            this.y += this.speedY + Math.random() * 50 - 25;
+            this.x += this.speedX + Math.random() * 20 - 10;
+            this.y += this.speedY + Math.random() * 20 - 10;
             this.history.push({x: this.x, y: this.y});
             if (this.history.length > this.maxLength) {
                 this.history.shift();
@@ -70,7 +94,7 @@ class Line {
 }
 
 const linesArray = [];
-const numberOfLines = 120;
+const numberOfLines = 50;
 
 for (let i = 0; i < numberOfLines; i ++) {
     linesArray.push(new Line(canvas));
