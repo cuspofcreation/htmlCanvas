7a4bb3cb7111a1aae2a8a114e241ee87233d6f7c
Alex Fisher <a.fisher615@gmail.com>
2023-11-28 21:42:30 +0900

    feat: draw constellation paths

--- flowFields - Particle Network/script.js
+++ flowFields - Particle Network/script.js
@@ -2,26 +2,31 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.fillStyle = 'white';
 console.log(ctx)
+
+const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+gradient.addColorStop(0, 'white');
+gradient.addColorStop(0.5, 'magenta');
+gradient.addColorStop(1, 'blue');
+ctx.fillStyle = gradient;
+ctx.strokeStyle = 'white';
 
 class Particle {
 
     constructor(effect) {
         this.effect = effect;
-        this.radius = Math.random() * 20 + 5;
+        this.radius = Math.random() * 5 + 2;
         this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2);
         this.y = this.radius + Math.random() * (this.effect.width - this.radius * 2);
-        this.vx = Math.random() * 5;
-        this.vy = Math.random() * 7;
+        this.vx = Math.random() * 2 - 0.5;
+        this.vy = Math.random() * 2 - 0.5;
     };
 
     draw(context) {
-        context.fillStyle = `hsl(${this.x * 0.5}, 100%, 50%`;
         context.beginPath();
         context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         context.fill();
-        context.stroke();
+        // context.stroke();
     };
 
     update() {
@@ -45,7 +50,7 @@ class Effect {
         this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.particles = [];
-        this.numberOfParticles = 200;
+        this.numberOfParticles = 150;
         this.createParticles();
     };
 
@@ -56,11 +61,33 @@ class Effect {
     };
 
     handleParticles(context) {
+        this.connectParticles(context);
         this.particles.forEach(particle => {
             particle.draw(context);
             particle.update();
         })
     };
+
+    connectParticles(context) {
+        const maxDist = 150;
+        for (let a = 0; a < this.particles.length; a++) {
+            for (let b = a; b < this.particles.length; b++) {
+                const dx = this.particles[a].x - this.particles[b].x;
+                const dy = this.particles[a].y - this.particles[b].y;
+                const dist = Math.hypot(dx, dy);
+                if (dist < maxDist) {
+                    context.save();
+                    const opacity = 1 - (dist/maxDist);
+                    context.globalAlpha = opacity;
+                    context.beginPath();
+                    context.moveTo(this.particles[a].x, this.particles[a].y);
+                    context.lineTo(this.particles[b].x, this.particles[b].y);
+                    context.stroke();
+                    context.restore();
+                }
+            }
+        }
+    }
 }
 
 const effect = new Effect(canvas);
