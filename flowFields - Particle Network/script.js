f9b7d5672bb467fc7b2b40463de0b783f16df2f1
Alex Fisher <a.fisher615@gmail.com>
2023-11-28 21:09:32 +0900

    feat: particle animations

--- flowFields - Particle Network/script.js
+++ flowFields - Particle Network/script.js
@@ -9,16 +9,33 @@ class Particle {
 
     constructor(effect) {
         this.effect = effect;
-        this.x = Math.random() * this.effect.width;
-        this.y = Math.random() * this.effect.height;
-        this.radius = 15;
+        this.radius = Math.random() * 20 + 5;
+        this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2);
+        this.y = this.radius + Math.random() * (this.effect.width - this.radius * 2);
+        this.vx = Math.random() * 5;
+        this.vy = Math.random() * 7;
     };
 
     draw(context) {
+        context.fillStyle = `hsl(${this.x * 0.5}, 100%, 50%`;
         context.beginPath();
         context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         context.fill();
+        context.stroke();
     };
+
+    update() {
+        this.x += this.vx;
+        this.y += this.vy;
+        
+        if (this.x > this.effect.width - this.radius || this.x < this.radius) {
+            this.vx *= -1;
+        }
+
+        if (this.y > this.effect.height - this.radius || this.y < this.radius) {
+            this.vy *= -1;
+        }
+    }
 };
 
 class Effect {
@@ -28,7 +45,7 @@ class Effect {
         this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.particles = [];
-        this.numberOfParticles = 20;
+        this.numberOfParticles = 200;
         this.createParticles();
     };
 
@@ -41,12 +58,17 @@ class Effect {
     handleParticles(context) {
         this.particles.forEach(particle => {
             particle.draw(context);
+            particle.update();
         })
     };
 }
 
 const effect = new Effect(canvas);
-effect.handleParticles(ctx);
 
 function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    effect.handleParticles(ctx);
+    requestAnimationFrame(animate);
 };
+
+animate();
